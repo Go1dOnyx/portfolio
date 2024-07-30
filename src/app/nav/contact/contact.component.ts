f267186d6b4b9b866cd9b615f89ff73d620e300c
Jesus Diaz <jdiazndca21@gmail.com>
2024-07-30 07:19:49 -0500

    fixed form validators

--- src/app/nav/contact/contact.component.ts
+++ src/app/nav/contact/contact.component.ts
@@ -15,16 +15,16 @@ export class ContactComponent implements OnInit {
         this.formData = this.builder.group({
             FullName: new FormControl('', [Validators.required]),
             Email: new FormControl('', [Validators.required, Validators.email]),
+            Phone: new FormControl('', [Validators.required, Validators.pattern('[0-9]{10}')]),
             Comment: new FormControl('', [Validators.required])
         });
     }
 
     submitForm(formData: any) {
-        if(formData?.valid) {
+        //add some if statements for each form property such as mobile if it contains - or no dash etc.
             console.log(formData?.value);
             console.log(formData.FullName);
             console.log(formData.Email);
             console.log(formData.Comment);
-        }
     }
 }
